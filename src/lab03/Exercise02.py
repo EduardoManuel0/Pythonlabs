003bf5de1f8f22459c88225c00a769a872ac0bbf
unknown <quissaco2000@gamil.com>
2025-10-16 09:41:09 +0100

    Commint Exersice 01

--- src/lab03/Exercise02.py
+++ src/lab03/Exercise02.py
@@ -6,32 +6,32 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from lib.text import normalize, tokenize, count_freq, top_n
 
 def stats(tableMode: bool = False):
-    print("Enter text (to finish, press Ctrl+Z on a new line): ")
-    text = sys.stdin.read()    # Reading all input from stdin
+    print("Introduza o texto (para finalizar, prima Ctrl+Z numa nova linha): ")
+    text = sys.stdin.read()    # Leitura de todas as entradas do stdin
     if not text.strip():
-        print("You have not entered any text!!!") #No text entered.
+        print("Não digitou nenhum texto!!!") #No text entered.
         return
-    normalized_text = normalize(text) # Normalizing the text.
-    tokens = tokenize(normalized_text) # Tokenizing the text.
+    normalized_text = normalize(text) # Normalizando o texto.
+    tokens = tokenize(normalized_text) # Tokenizando o texto.
     # Calculating statistics
-    total_words = len(tokens)  # Calculate the total words.
-    unique_words = len(set(tokens)) #Get the total of words without repetitions.
-    # Frequencies and Top 5
+    total_words = len(tokens)  # Calcule o total de palavras.
+    unique_words = len(set(tokens)) #Obtenha o total de palavras sem repetições.
+    # Frequências e Top 5
     freq = count_freq(tokens)
     top_words = top_n(freq)
     if not tableMode:        
-        # Вывод результатов
-        print(f"Total of words: {total_words}")
+        #Os seus resultados
+        print(f"Total de palavras: {total_words}")
         print(f"Total of Unique words: {unique_words}")
-        print("Top-5:")
+        print("Top 5:")
         for word, count in top_words:
             print(f"{word}:{count}")
     else:       
        max_word_len = max(len(word) for word, _ in top_words)
-       print(f'{"Word":<{max_word_len}} | Frequency')
+       print(f'{"Word":<{max_word_len}} | Frequência')
        print("-" * (max_word_len + 14))
        for word, count in top_words:
             print(f"{word:<{max_word_len}} | {count}")
 
-if __name__ == "__main__":
+if __name__ == "__principal__":
     stats()
